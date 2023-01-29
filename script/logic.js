1c68fcbbe561db7024799fa38780b168cf3d046b
miuroku <thebestguy1101001@gmail.com>
2023-01-29 18:42:38 +0300

    redirect added

--- script/logic.js
+++ script/logic.js
@@ -7,31 +7,31 @@ function main() {
 
   // Modal window logic 
 
-  // Get the modal
-  var modal = document.getElementById("myModal");
+  // // Get the modal
+  // var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("button-strategy");
+  // // Get the button that opens the modal
+  // var btn = document.getElementById("button-strategy");
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  // // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+  // // When the user clicks on the button, open the modal
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  // // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+  // // When the user clicks anywhere outside of the modal, close it
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
 
   // Process sending to server.
   // $("#plan-form").on( "submit", function( event ) {    
@@ -42,6 +42,8 @@ function main() {
   //       console.log(data);
   //   });
   // });
+
+  
 
 }
 
