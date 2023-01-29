da25c6ce39468d0f8c4dddde079c29a4f47e7b8e
miuroku <thebestguy1101001@gmail.com>
2023-01-29 14:32:48 +0300

    get btn by name

--- script/logic.js
+++ script/logic.js
@@ -1,3 +1,7 @@
 console.log('Test scripts executions on github pages page');
 
+var btn = getElementsByClassName('button-strategy');
+
+console.log('Our btn : ', JSON.stringify(btn, null, 2));
+
 alert('Hola');
